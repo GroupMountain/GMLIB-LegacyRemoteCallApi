78f831cef6d956525c828afe728db19f8fecb518
Tsubasa6848 <116721335+Tsubasa6848@users.noreply.github.com>
2024-02-15 17:00:02 +0800

    feat: more api

--- src/CompatibilityApi.cpp
+++ src/CompatibilityApi.cpp
@@ -13,4 +13,14 @@ void Export_Compatibility_API() {
     RemoteCall::exportAs("GMLIB_API", "getAllPlayerUuids", []() -> std::vector<std::string> {
         return GMLIB_Player::getAllUuids();
     });
+    RemoteCall::exportAs("GMLIB_API", "getAllExperiments", []() -> std::vector<int> {
+        return {6, 7, 8, 9, 10, 13, 16, 17, 18};
+    });
+    RemoteCall::exportAs("GMLIB_API", "getExperimentTranslatedName", [](int id) -> std::string {
+        auto map = GMLIB_Level::getAllExperimentsTranslateKeys();
+        if (map.count(id)) {
+            return map[id];
+        }
+        return "";
+    });
 }
